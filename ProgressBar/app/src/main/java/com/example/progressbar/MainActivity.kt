04e7156c254da84ecfa04f5211b7ac08b40f872f
aranzabe <faranzabe@gmail.com>
2023-10-16 12:28:35 +0200

    ProgressBar

---
+++ ProgressBar/app/src/main/java/com/example/progressbar/MainActivity.kt
@@ -0,0 +1,83 @@
+package com.example.progressbar
+
+import android.os.Bundle
+import android.widget.ProgressBar
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.progressbar.ui.theme.ProgressBarTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ProgressBarTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    //MiProgressBar()
+                    MiProgressBarAvanzado()
+                }
+            }
+        }
+    }
+}
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun MiProgressBar() {
+    var showPbVal by rememberSaveable { mutableStateOf(false) }
+    Column {
+        if (showPbVal) {
+            CircularProgressIndicator(color = Color.Blue, strokeWidth = 3.dp)
+            LinearProgressIndicator(color = Color.Cyan, trackColor = Color.Green)
+        }
+        Button(onClick = { showPbVal = !showPbVal}) {
+            if (showPbVal){
+                Text("Desahibilitar")
+            }
+            else {
+                Text("Habilitar")
+            }
+        }
+    }
+}
+
+@Composable
+fun MiProgressBarAvanzado() {
+    var valor by rememberSaveable { mutableStateOf(0f) }
+    Column {
+        CircularProgressIndicator(color = Color.Blue, strokeWidth = 3.dp, progress = valor)
+        LinearProgressIndicator(color = Color.Cyan, trackColor = Color.Green, progress = valor)
+        Row() {
+            Button(onClick = { valor += 0.1f }) {
+                Text("Incrementar")
+            }
+            Button(onClick = { valor -= 0.1f }) {
+                Text("Decrementar")
+            }
+        }
+    }
+}
